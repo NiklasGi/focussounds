09c27a4b12af2d48a3bc706c403434c8e52ef94b
NiklasGi <niklas.gittel@gmail.com>
2026-08-08 06:47:44 +0200

    feat: added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,9 +22,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col p-8">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex items-center justify-between pl-16 text-white">
+            <h1 className="text-2xl font-black">FocusSounds</h1>
+        </header>
+    );
+};
+
+export default Header;
