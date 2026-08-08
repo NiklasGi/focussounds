5ad603fc7d7869164ed14b39324a0f0f0a7d462c
NiklasGi <niklas.gittel@gmail.com>
2026-08-08 15:33:28 +0200

    feat: styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
-      <body className="min-h-full flex flex-col p-8">
+      <body className="min-h-full h-screen flex flex-col p-12">
         <Header />
         {children}
       </body>
