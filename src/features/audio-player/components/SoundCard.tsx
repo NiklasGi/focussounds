f670f3d06e1eb7451effb69c0a69fb786b507661
NiklasGi <niklas.gittel@gmail.com>
2026-08-10 18:56:11 +0200

    styling: updated soundcard size

--- src/features/audio-player/components/SoundCard.tsx
+++ src/features/audio-player/components/SoundCard.tsx
@@ -12,7 +12,7 @@ const SoundCard = ({ sound, isActive, onClick }: SoundCardProps) => {
     const cardTheme = isActive ? "bg-zinc-100 text-black" : undefined;
 
     return (
-        <Card className={`max-h-36 min-h-36 h-36 w-36 flex font-semibold flex-col items-start justify-end p-4 cursor-pointer transition-colors duration-300 ${cardTheme}`} onClick={onClick}>
+        <Card className={`max-h-32 min-h-32 h-32 w-32 flex font-semibold flex-col items-start justify-end p-4 cursor-pointer transition-colors duration-300 ${cardTheme}`} onClick={onClick}>
             {isActive && <PlayingIndicator />}
             <h3>{sound.title}</h3>
         </Card>
